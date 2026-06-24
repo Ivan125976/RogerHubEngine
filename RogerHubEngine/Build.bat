dedd6039871f663607ed357307cd5cb1fab841ff
Axolotl512 <ivanprogrammist2345@mail.ru>
2026-06-25 00:23:50 +0300

    Добавление сборщика

---
+++ RogerHubEngine/Build.bat
@@ -0,0 +1,20 @@
+@echo off
+set VERSION=3.0.0
+echo RogerHubEngine app builder
+echo === build .NET ===
+
+dotnet publish RogerHubEngine.csproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true
+dotnet publish RogerHubEngine.csproj -c Release -r win-arm64 --self-contained true -p:PublishSingleFile=true
+dotnet publish RogerHubEngine.csproj -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true
+dotnet publish RogerHubEngine.csproj -c Release -r linux-arm64 --self-contained true -p:PublishSingleFile=true
+
+echo === build VPK ===
+
+vpk pack --runtime win-x64 --packId Yocto_Roger-win-x64 --packVersion %VERSION% --packDir bin\Release\net9.0\win-x64\publish --mainExe Yocto_Roger.exe
+
+vpk pack --runtime win-arm64 --packId Yocto_Roger-win-arm64 --packVersion %VERSION% --packDir bin\Release\net9.0\win-arm64\publish --mainExe Yocto_Roger.exe
+
+echo === build linux zip ===
+
+powershell Compress-Archive -Force bin\Release\net9.0\linux-x64\publish Releases\RogerHubEngine-linux-x64-%VERSION%.zip
+powershell Compress-Archive -Force bin\Release\net9.0\linux-arm64\publish Releases\RogerHubEngine-linux-arm64-%VERSION%.zip
