878bc8987a302f233f95a718709e10f9cced7d2d
d3ath-script <jacky50332@gmail.com>
2026-06-25 18:24:09 +0300

    Переписал Build-батник, скоро напишу .sh скрипт, под линукс

--- Yocto_Roger/Build.bat
+++ Yocto_Roger/Build.bat
@@ -7,17 +7,29 @@ dotnet publish Yocto_Roger.csproj -c Release -r win-x64 --self-contained true -p
 dotnet publish Yocto_Roger.csproj -c Release -r win-arm64 --self-contained true -p:PublishSingleFile=true
 dotnet publish Yocto_Roger.csproj -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true
 dotnet publish Yocto_Roger.csproj -c Release -r linux-arm64 --self-contained true -p:PublishSingleFile=true
+dotnet publish Yocto_Roger.csproj -c Release -r osx-x64 --self-contained true -p:PublishSingleFile=true
+
+echo === Installing VPK-tool ===
+
+dotnet tool install -g vpk
 
 echo === build VPK ===
 
+echo Building win-x64
 vpk pack --runtime win-x64 --packId Yocto_Roger-win-x64 --packVersion %VERSION% --packDir bin\Release\net9.0\win-x64\publish --mainExe Yocto_Roger.exe
 
+echo Building win-arm64
 vpk pack --runtime win-arm64 --packId Yocto_Roger-win-arm64 --packVersion %VERSION% --packDir bin\Release\net9.0\win-arm64\publish --mainExe Yocto_Roger.exe
 
-echo === build linux zip ===
+echo Building osx-x64 (mac)
+echo If you're compiling on Windows, it won't work because it requires tools that are only available on Mac. Therefore, if you want to compile a binary for Mac, please compile it on a Mac
+vpk [osx] pack --runtime osx-x64 --packId Yocto_Roger-osx-x64 --packVersion %VERSION% --packDir bin\Release\net9.0\osx-x64\publish\Yocto_Roger.app --mainExe Yocto_Roger
 
-powershell Compress-Archive -Force bin\Release\net9.0\linux-x64\publish Releases\Yocto_Roger-linux-x64-%VERSION%.zip
-powershell Compress-Archive -Force bin\Release\net9.0\linux-arm64\publish Releases\Yocto_Roger-linux-arm64-%VERSION%.zip
+echo Building linux-x64
+vpk [linux] pack --runtime linux-x64 --packId Yocto_Roger-linux-x64 --packVersion %VERSION% --packDir bin\Release\net9.0\linux-x64\publish --mainExe Yocto_Roger
+
+echo Building linux-arm64
+vpk [linux] pack --runtime linux-arm64 --packId Yocto_Roger-win-arm64 --packVersion %VERSION% --packDir bin\Release\net9.0\linux-arm64\publish --mainExe Yocto_Roger
 
 echo done.
 pause
