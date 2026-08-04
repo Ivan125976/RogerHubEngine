ae839765f4412c2ead8df00f3eeaa3867c650f0d
d3ath-script <jacky50332@gmail.com>
2026-08-04 08:42:05 +0300

    Почистил то, что забыл почистить, извиняюсь

--- Yocto_Roger/Initialization.cs
+++ Yocto_Roger/Initialization.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Yocto_Roger.RogerCore;
+﻿using Yocto_Roger.RogerCore;
 using Yocto_Roger.UI.Interfaces;
 
 namespace Yocto_Roger
